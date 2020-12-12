fb993c1fc41a0584a4e900298695a80cbe7715f0
zalmek <zalmek23@gmail.com>
2020-12-12 20:00:18 +0300

    0.3.1 Version small code fix

--- app/src/main/java/com/example/chargemessenger/MyBroadcastReceiver.java
+++ app/src/main/java/com/example/chargemessenger/MyBroadcastReceiver.java
@@ -1,7 +1,6 @@
 package com.example.chargemessenger;
 
 import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -17,34 +16,10 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 
-import static android.content.Context.NOTIFICATION_SERVICE;
-
 public class MyBroadcastReceiver extends BroadcastReceiver {
     Boolean charger=false;
     @Override
     public void onReceive(Context context, Intent intent) {// +- работает
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-
-        StrictMode.setThreadPolicy(policy);
-        String urlString = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s ";
-
-        String apiToken = "1448041949:AAGKZXLqa7MTi25uE3JflofJrFadzY0KQSc";
-
-        // really need a func
-        String chatId = "904847378";
-        String text = "Hello_world123!";
-
-        urlString = String.format(urlString, apiToken, chatId, text);
-
-        try {
-            URL url = new URL(urlString);
-            URLConnection conn = url.openConnection();
-            InputStream is = new BufferedInputStream(conn.getInputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (Exception e){
-            e.printStackTrace();
-        }
 
         if (intent.getAction().equals(Intent.ACTION_POWER_CONNECTED)) {
             Toast.makeText(context, "The device is charging", Toast.LENGTH_SHORT).show();
@@ -93,25 +68,39 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
         Notification noti;
         PendingIntent pIntent = PendingIntent.getActivity(context, 0, notify, 0);
 
-        if(curLevel <= 15 && !charger) {
-            noti = new Notification.Builder(context).setTicker("Ticker Title")
-                    .setContentTitle("Battery Low")
-                    .setContentText("Battery " + curLevel + "% Please Connect Charger")
-                    .setSmallIcon(R.mipmap.ic_launcher)
-                    .setContentIntent(pIntent).getNotification();
-            noti.flags = Notification.FLAG_AUTO_CANCEL;
-            NotificationManager nm = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
-            nm.notify(0, noti);
-        }
-        else if(curLevel == 100 && charger){
-            noti = new Notification.Builder(context).setTicker("Ticker Title")
-                    .setContentTitle("Battery Okay")
-                    .setContentText("Battery full Please disconnect Charger")
-                    .setSmallIcon(R.mipmap.ic_launcher)
-                    .setContentIntent(pIntent).getNotification();
-            noti.flags = Notification.FLAG_AUTO_CANCEL;
-            NotificationManager nm = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
-            nm.notify(0, noti);
+//        if(curLevel <= 15 && !charger) {
+//            noti = new Notification.Builder(context).setTicker("Ticker Title")
+//                    .setContentTitle("Battery Low")
+//                    .setContentText("Battery " + curLevel + "% Please Connect Charger")
+//                    .setSmallIcon(R.mipmap.ic_launcher)
+//                    .setContentIntent(pIntent).getNotification();
+//            noti.flags = Notification.FLAG_AUTO_CANCEL;
+//            NotificationManager nm = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+//            nm.notify(0, noti);
+//        }
+         if(curLevel == 100 && charger){
+             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+             StrictMode.setThreadPolicy(policy);
+             String urlString = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s ";
+
+             String apiToken = "1448041949:AAGKZXLqa7MTi25uE3JflofJrFadzY0KQSc";
+
+             // really need a func
+             String chatId = "904847378";
+             String text = "Hello_world123!";
+
+             urlString = String.format(urlString, apiToken, chatId, text);
+
+             try {
+                 URL url = new URL(urlString);
+                 URLConnection conn = url.openConnection();
+                 InputStream is = new BufferedInputStream(conn.getInputStream());
+             } catch (IOException e) {
+                 e.printStackTrace();
+             } catch (Exception e){
+                 e.printStackTrace();
+             }
         }
 
     }
