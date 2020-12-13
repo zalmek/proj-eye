f29e25a90915244a1b31a30e5c39eb60e01406e0
zalmek <zalmek23@gmail.com>
2020-12-13 22:58:16 +0300

    0.4 overwrite id file in second thread

--- app/src/main/java/com/example/chargemessenger/MainActivity.java
+++ app/src/main/java/com/example/chargemessenger/MainActivity.java
@@ -2,16 +2,45 @@ package com.example.chargemessenger;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.chargemessenger.databinding.ActivityMainBinding;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 
 public class MainActivity extends AppCompatActivity {
+    class IThread extends Thread
+    {
+        @Override
+        public void run()	//Этот метод будет выполнен в побочном потоке
+        {
+            String rootDataDir = getExternalFilesDir(Environment.getDataDirectory().getAbsolutePath()).getAbsolutePath();
+            Log.i(TAG, rootDataDir.toString());
+            File file = new File(rootDataDir, "id.txt");
+            try {
+                PrintWriter writer = new PrintWriter(new FileWriter(file));
+                writer.printf("%s", binding.Userid.getText()); //Записываем текст в файл
+                writer.close(); // Закрываем файл
+//                Log.i(TAG, String.valueOf(file.canRead()));
+//                Log.i(TAG, String.valueOf(file.canWrite()));
+                Log.i(TAG, String.valueOf(file.exists()));
+            } catch (IOException e) {
+                e.printStackTrace();
+                file.exists();
+            }
+        }
+    }
+    static IThread secondThread;
     public ActivityMainBinding binding;
+    private static final String TAG = "myLogs";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +50,10 @@ public class MainActivity extends AppCompatActivity {
         binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MainActivity.this.startMyService();
-                Toast.makeText(getApplicationContext(),"buttonclicked",Toast.LENGTH_SHORT).show();
+                secondThread = new IThread();
+                secondThread.start();
+//                MainActivity.this.startMyService();
+//                Toast.makeText(getApplicationContext(),"buttonclicked",Toast.LENGTH_SHORT).show();
             }
         });
     }
