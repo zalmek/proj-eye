07e705685be9af3ed97d919ef407bd9210436e99
zalmek <zalmek23@gmail.com>
2021-03-08 16:47:36 +0300

    Fixes

--- app/src/main/java/com/example/chargemessenger/MainActivity.java
+++ app/src/main/java/com/example/chargemessenger/MainActivity.java
@@ -1,17 +1,13 @@
 package com.example.chargemessenger;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
 
 import com.example.chargemessenger.databinding.ActivityMainBinding;
 
@@ -20,22 +16,20 @@ import dagger.hilt.android.AndroidEntryPoint;
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
     public ActivityMainBinding binding;
-    private static final int NOTIFY_ID = 229;
-    // Идентификатор канала
-    private static String CHANNEL_ID = "Chargemes channel";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
+        startService(new Intent(this,MyService.class));
         setContentView(view);
-        Fragment fragment = new EnterFragment();
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
         EnterFragment enterFragment = new EnterFragment();
-        ft.replace(R.id.activityid, enterFragment);
-        binding.progressBar.setVisibility(View.GONE);
-        ft.commit();
+        transaction.replace(R.id.activityid, enterFragment);
+        transaction.commit();
+
     }
 
     @Override
