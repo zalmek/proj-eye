f3cce3e74798d2016e793c8f1f0de66e9219ddaf
zalmek <zalmek23@gmail.com>
2021-03-09 16:16:09 +0300

    Fix

--- app/src/main/java/com/example/chargemessenger/BatLvlFragment.java
+++ app/src/main/java/com/example/chargemessenger/BatLvlFragment.java
@@ -6,16 +6,26 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.chargemessenger.databinding.FragmentBatLvlBinding;
 
+import org.jetbrains.annotations.NotNull;
+
 public class BatLvlFragment extends Fragment {
-    private FragmentBatLvlBinding batLvlBinding;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        batLvlBinding = FragmentBatLvlBinding.inflate(inflater, container, false);
+        com.example.chargemessenger.databinding.FragmentBatLvlBinding batLvlBinding = FragmentBatLvlBinding.inflate(inflater, container, false);
+        batLvlBinding.extendedFab.setOnClickListener(v ->
+        {
+            FragmentManager fragmentManager = getParentFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.activityid,new EnterFragment() );
+            transaction.commit();
+        });
         batLvlBinding.waveView.setProgress(23);
         return batLvlBinding.getRoot();
     }
