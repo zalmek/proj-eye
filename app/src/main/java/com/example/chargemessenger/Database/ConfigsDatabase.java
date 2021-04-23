100750ddb1ceb675c19d9fe4fdd68b11182343e9
zalmek <zalmek23@gmail.com>
2021-04-23 17:33:07 +0300

    not work

--- app/src/main/java/com/example/chargemessenger/Database/ConfigsDatabase.java
+++ app/src/main/java/com/example/chargemessenger/Database/ConfigsDatabase.java
@@ -1,4 +1,54 @@
 package com.example.chargemessenger.Database;
 
-public class ConfigsDatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.chargemessenger.Database.Dao.ConfigDao;
+import com.example.chargemessenger.MVVM.Models.Configs;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.inject.Singleton;
+
+import dagger.Provides;
+import dagger.hilt.android.qualifiers.ApplicationContext;
+
+@Database(entities = {Configs.class}, version = 1, exportSchema = false)
+public abstract class ConfigsDatabase extends RoomDatabase {
+    public abstract ConfigDao configsDao();
+
+    private static final int NUMBER_OF_THREADS = 4;
+
+    public static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    private static ConfigsDatabase instance = null;
+
+    // CAN CAUSE MISTAKE CHANGE COMMEND NEXT TO LINES
+    @Singleton
+    @Provides
+    public static ConfigsDatabase getDatabase(@ApplicationContext Context context) {
+                    return Room.databaseBuilder(context,
+                            ConfigsDatabase.class,
+                            "configs_database.db")
+                            .build();
+    }
+
+
+//    public static ConfigsDatabase getDatabase(@ApplicationContext Context context) {
+//        if (instance == null) {
+//            synchronized (ConfigsDatabase.class) {
+//                if (instance == null) {
+//                    instance = Room.databaseBuilder(context,
+//                            ConfigsDatabase.class,
+//                            "configs_database.db")
+//                            .build();
+//                }
+//            }
+//        }
+//        return instance;
+//    }
 }
--- app/src/main/java/com/example/chargemessenger/Database/Dao/ConfigDao.java
+++ app/src/main/java/com/example/chargemessenger/Database/Dao/ConfigDao.java
@@ -1,4 +1,23 @@
 package com.example.chargemessenger.Database.Dao;
 
-public class ConfigDao {
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.chargemessenger.MVVM.Models.Configs;
+
+import java.util.List;
+
+@Dao
+public interface ConfigDao {
+    @Insert
+    void insert(Configs configs);
+
+    @Update
+    void update(Configs config);
+
+    @Query("SELECT * FROM configs")
+    LiveData<List<Configs>> getAllConfigs();
 }
